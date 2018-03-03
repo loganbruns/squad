d593ee6e16985d6c1613537467611c56964bf641
Logan O'Sullivan Bruns <logan@gedanken.org>
2018-03-03 14:27:03 -0500

    v4_1: Multiheaded Attention updated to fix weighted sum to be full size values.

--- code/modules.py
+++ code/modules.py
@@ -201,6 +201,72 @@ def masked_softmax(logits, mask, dim):
     return masked_logits, prob_dist
 
 
+class HeadAttn(object):
+    """Module for scaled head attention.
+
+    This only differs from BasicAttn in that it does most of its
+    operations with a scaled set of vectors then returns the full size
+    weighted sum.
+
+    Note: in this module we use the terminology of "keys" and "values" (see lectures).
+    In the terminology of "X attends to Y", "keys attend to values".
+
+    In the baseline model, the keys are the context hidden states
+    and the values are the question hidden states.
+
+    We choose to use general terminology of keys and values in this module
+    (rather than context and question) to avoid confusion if you reuse this
+    module with other inputs.
+
+    """
+
+    def __init__(self, keep_prob, key_vec_size, value_vec_size):
+        """
+        Inputs:
+          keep_prob: tensor containing a single scalar that is the keep probability (for dropout)
+          key_vec_size: size of the key vectors. int
+          value_vec_size: size of the value vectors. int
+        """
+        self.keep_prob = keep_prob
+        self.key_vec_size = key_vec_size
+        self.value_vec_size = value_vec_size
+
+    def build_graph(self, scaled_values, values, values_mask, keys):
+        """
+        Keys attend to values.
+        For each key, return an attention distribution and an attention output vector.
+
+        Inputs:
+          values: Tensor shape (batch_size, num_values, value_vec_size).
+          values_mask: Tensor shape (batch_size, num_values).
+            1s where there's real input, 0s where there's padding
+          keys: Tensor shape (batch_size, num_keys, value_vec_size)
+
+        Outputs:
+          attn_dist: Tensor shape (batch_size, num_keys, num_values).
+            For each key, the distribution should sum to 1,
+            and should be 0 in the value locations that correspond to padding.
+          output: Tensor shape (batch_size, num_keys, hidden_size).
+            This is the attention output; the weighted sum of the values
+            (using the attention distribution as weights).
+        """
+        with vs.variable_scope("HeadAttn"):
+
+            # Calculate attention distribution
+            values_t = tf.transpose(scaled_values, perm=[0, 2, 1]) # (batch_size, value_vec_size, num_values)
+            attn_logits = tf.matmul(keys, values_t) # shape (batch_size, num_keys, num_values)
+            attn_logits_mask = tf.expand_dims(values_mask, 1) # shape (batch_size, 1, num_values)
+            _, attn_dist = masked_softmax(attn_logits, attn_logits_mask, 2) # shape (batch_size, num_keys, num_values). take softmax over values
+
+            # Use attention distribution to take weighted sum of values
+            output = tf.matmul(attn_dist, values) # shape (batch_size, num_keys, value_vec_size)
+
+            # Apply dropout
+            output = tf.nn.dropout(output, self.keep_prob)
+
+            return attn_dist, output
+
+
 class MultiHeadedAttn(object):
     """Module for multiheaded attention.
 
@@ -227,7 +293,7 @@ class MultiHeadedAttn(object):
         self.value_vec_size = value_vec_size
         self.num_values = num_values
         self.num_heads = num_heads
-        self.scaled_attn = [BasicAttn(keep_prob, key_vec_size / num_heads, value_vec_size / num_heads) for _ in range(num_heads)]
+        self.scaled_attn = [HeadAttn(keep_prob, key_vec_size / num_heads, value_vec_size / num_heads) for _ in range(num_heads)]
 
     def build_graph(self, values, values_mask, keys):
         """
@@ -263,7 +329,7 @@ class MultiHeadedAttn(object):
             scaled_values = tf.unstack(tf.reshape(tf.tensordot(values, W_values, 1), shape), axis=3)
 
             # shape (batch_size, num_keys, hidden_size, num_heads)
-            outputs = tf.stack([self.scaled_attn[i].build_graph(scaled_values[i], values_mask, scaled_keys[i])[1] for i in range(self.num_heads)], axis=3)
+            outputs = tf.stack([self.scaled_attn[i].build_graph(scaled_values[i], values, values_mask, scaled_keys[i])[1] for i in range(self.num_heads)], axis=3)
 
             # shape (batch_size, num_keys, hidden_size)
-            return tf.reshape(outputs, (-1, self.num_values, self.value_vec_size))
+            return tf.reduce_sum(outputs / self.num_heads, axis=3)
