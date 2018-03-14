8057dbe99a963605b9d5471f29c18867f87e65fa
Logan O'Sullivan Bruns <logan@gedanken.org>
2018-03-14 08:45:50 -0400

    Switch to CudnnCompatibleLSTMCell which seems much faster.

--- code/modules.py
+++ code/modules.py
@@ -47,7 +47,8 @@ class RNNEncoder(object):
         self.keep_prob = keep_prob
         self.layers = layers
         def lstm_cell():
-            return DropoutWrapper(rnn_cell.BasicLSTMCell(self.hidden_size), input_keep_prob=self.keep_prob)
+#            return DropoutWrapper(rnn_cell.BasicLSTMCell(self.hidden_size), input_keep_prob=self.keep_prob)
+            return DropoutWrapper(tf.contrib.cudnn_rnn.CudnnCompatibleLSTMCell(self.hidden_size), input_keep_prob=self.keep_prob)
         self.rnn_cell_fw = rnn_cell.MultiRNNCell([lstm_cell() for _ in xrange(self.layers)])
         self.rnn_cell_bw = rnn_cell.MultiRNNCell([lstm_cell() for _ in xrange(self.layers)])
 
@@ -68,7 +69,7 @@ class RNNEncoder(object):
 
             # Note: fw_out and bw_out are the hidden states for every timestep.
             # Each is shape (batch_size, seq_len, hidden_size).
-            (fw_out, bw_out), _ = tf.nn.bidirectional_dynamic_rnn(self.rnn_cell_fw, self.rnn_cell_bw, inputs, input_lens, dtype=tf.float32)
+            (fw_out, bw_out), _ = tf.nn.bidirectional_dynamic_rnn(self.rnn_cell_fw, self.rnn_cell_bw, inputs, input_lens, swap_memory=True, dtype=tf.float32)
 
             # Concatenate the forward and backward hidden states
             out = tf.concat([fw_out, bw_out], 2)
